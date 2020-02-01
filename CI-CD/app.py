97685aa1e7dfe66c7614f3132cb1e62f3f8dbbf8
Yael <yael346@gmail.com>
2020-02-01 22:28:41 +0200

    fix bug: update requirement + change server's replies + expose port

--- CI-CD/app.py
+++ CI-CD/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return 'GO AWAY!'
 
 
 @app.route('/goaway')
 def goaway():
-    return 'GO AWAY!'
+    return 'Welcome to the coolest server everrrrrrr :)'
 
 
 if __name__ == '__main__':
