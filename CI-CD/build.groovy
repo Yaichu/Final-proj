8253b8a6c10c53fc9b63011c8bbab880e764ad85
Yael <yael346@gmail.com>
2020-02-02 17:07:45 +0200

    add expose app stage

--- CI-CD/build.groovy
+++ CI-CD/build.groovy
@@ -1,6 +1,6 @@
 node ("linux") {
     def DockerImage = "proj_app"
-    stage('Git') { // Get code from GitLab repository
+    stage("Git") { // Get code from GitLab repository
         git branch: 'master',
             url: 'https://github.com/Yaichu/Mid-proj.git'
     }
@@ -19,7 +19,11 @@ node ("linux") {
         }
     }
     
-    stage('Deploy') { // Deploy the app
+    stage("deploy") { // Deploy the app
         kubernetesDeploy(kubeconfigId: 'e17f6f2f-5636-4ed1-a2e3-180aa99403e9', configs: 'CI-CD/k8sdeploy.yml', enableConfigSubstitution: true)
+    }
+    
+    stage("expose service") { // Expose the app to the world
+        kubernetesDeploy(kubeconfigId: 'e17f6f2f-5636-4ed1-a2e3-180aa99403e9', configs: 'CI-CD/k8sservice.yml', enableConfigSubstitution: true)
     }
 }
